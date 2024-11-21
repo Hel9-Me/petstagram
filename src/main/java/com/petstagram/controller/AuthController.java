769a3762074a9f9be3b6bbbb9a774c4727fbad4c
gyoungsubLee <dltjq2323@gmail.com>
2024-11-21 11:39:43 +0900

    refact: 로그아웃 api - http 메서드 수정 • POST -> GET

--- src/main/java/com/petstagram/controller/AuthController.java
+++ src/main/java/com/petstagram/controller/AuthController.java
@@ -9,6 +9,7 @@ import com.petstagram.model.entity.User;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -55,7 +56,7 @@ public class AuthController {
         return new ResponseEntity<>("로그인이 성공 했습니다. ",HttpStatus.OK);
     }
 
-    @PostMapping("/logout")
+    @GetMapping("/logout")
     public ResponseEntity<String> logout(HttpServletRequest request){
 
         HttpSession session = request.getSession(false);
