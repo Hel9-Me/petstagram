bfa6d859e6dd8ca8c8e512d2dfff790e457681eb
gyoungsubLee <dltjq2323@gmail.com>
2024-11-22 13:14:43 +0900

    refact: password validation 패턴 추가

--- src/main/java/com/petstagram/dto/auth/LoginRequestDto.java
+++ src/main/java/com/petstagram/dto/auth/LoginRequestDto.java
@@ -2,6 +2,7 @@ package com.petstagram.dto.auth;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,7 +12,9 @@ public class LoginRequestDto {
     @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$", message = "올바른 이메일 형식을 입력해 주세요")
     private String email;
 
-    @NotBlank // 패턴 논의 전
+
+    @Size(min = 8, message = "비밀번호는 8글자 이상으로 입력해주세요.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$", message = "비밀번호는 대소문자 포함 영문 + 숫자 + 특수문자 최소 1글자씩 입력해주세요.")
     private String password;
 
     public LoginRequestDto(String email, String password) {
--- src/main/java/com/petstagram/dto/auth/SignUpRequestDto.java
+++ src/main/java/com/petstagram/dto/auth/SignUpRequestDto.java
@@ -2,6 +2,7 @@ package com.petstagram.dto.auth;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -14,8 +15,8 @@ public class SignUpRequestDto {
     @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$", message = "올바른 이메일 형식을 입력해 주세요")
     private String email;
 
-    // 패턴 논의되지 않았음, 우선 보류
-    @NotBlank
+    @Size(min = 8, message = "비밀번호는 8글자 이상으로 입력해주세요.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$", message = "비밀번호는 대소문자 포함 영문 + 숫자 + 특수문자 최소 1글자씩 입력해주세요.")
     private String password;
 
 
